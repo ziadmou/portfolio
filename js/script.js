139f448e0ca239b0a11c0142df4017ae2d9feb9e
ziadmou <faroukziadmoustafa@gmail.com>
2026-09-25 16:41:52 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const els = document.querySelectorAll('.reveal');
+  const io = new IntersectionObserver((entries)=>{
+    entries.forEach(e=>{ if(e.isIntersecting){ e.target.classList.add('in'); io.unobserve(e.target); } });
+  }, { threshold:.15 });
+  els.forEach(el=>io.observe(el));
